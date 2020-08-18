61a96b10365d9e25ae8917c39e89ce38715de66f
Aditya Gupta <ag15035@gmail.com>
2020-08-19 00:37:47 +0530

    svg

--- svgCreator.js
+++ svgCreator.js
@@ -4,10 +4,8 @@ let folderName = 'outputSVGs'
 mkdir(folderName)
 
 for (let i = 1; i <= 100; ++i) {
-    let svgContent = `<svg height="15" width="250" xmlns::xlink="http://www.w3.org/1999/xlink"">
-        <a xlink:href="https://github.com/AdityaGupta150/100DaysOfCode" target="_blank">
-            <text x="0" y="15" fill="red">#${i}</text>
-        </a>
+    let svgContent = `<svg height="15" width="250">
+        <text x="0" y="15" fill="red">#${i}</text>
     </svg>`
     writeFile(`${folderName}/${i}.svg`, svgContent)
         .then(() => console.log('🎉 Done!!'))
